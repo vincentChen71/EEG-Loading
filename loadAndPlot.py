e1dcfd7b9b83a48f6d7571a9557dba2ce5ecebdd
Vincent Chen <vchen7142@gmail.com>
2026-05-21 12:20:28 -0500

    Update loadAndPlot.py
    Plotted band power and fixed up some axes

--- loadAndPlot.py
+++ loadAndPlot.py
@@ -18,13 +18,14 @@ for i in range(NUM_CHANNELS):
     x = loadedData[0,i,:]
     plt.plot(f,x)
 
-plt.ylim([0, NUM_FREQS])
+plt.ylim([0, 60])
+plt.xlabel("Frequency (Hz)")
 plt.title("Raw Data")
 
 # Set Up Slider
 slider_color = 'White'
 
-axis_position = plt.axes([0.2,0.03,0.65,0.03], facecolor = slider_color)
+axis_position = plt.axes([0.2,0,0.65,0.03], facecolor = slider_color)
 slider_position = Slider(axis_position, 'Time', 0, NUM_TIMESTEPS, valinit=0, valstep=range(250))
 
 def update(val):
@@ -33,7 +34,8 @@ def update(val):
     for i in range(NUM_CHANNELS):
         x = loadedData[pos,i,:]
         Axis.plot(f,x)
-    Axis.set_ylim(0,NUM_FREQS)
+    Axis.set_ylim(0,60)
+    Axis.set_xlabel("Frequency (Hz)")
     Axis.set_title("Raw Data")
     Plot.canvas.draw_idle()
     
@@ -51,12 +53,13 @@ for i in range(NUM_CHANNELS):
     plt.plot(f,denoisedData[0,i,:])
 
 plt.ylim([0,60])
+plt.xlabel("Frequency (Hz)")
 plt.title("Denoised Data")
 
 # Setting up slider again
 slider_color = 'white'
 
-axis_position = plt.axes([0.2,0.03,0.65,0.03], facecolor = slider_color)
+axis_position = plt.axes([0.2,0,0.65,0.03], facecolor = slider_color)
 slider_position2 = Slider(axis_position, 'Time', 0, NUM_TIMESTEPS, valinit=0, valstep=range(250))
 
 def updateDenoised(val):
@@ -65,10 +68,32 @@ def updateDenoised(val):
     for i in range(NUM_CHANNELS):
         x = denoisedData[pos,i,:]
         Axis2.plot(f,x)
-    Axis2.set_ylim(0,NUM_FREQS)
+    Axis2.set_ylim(0,60)
+    Axis2.set_xlabel("Frequency (Hz)")
     Axis2.set_title("Denoised Data")
     Plot2.canvas.draw_idle()
     
 slider_position2.on_changed(updateDenoised)
+
+# Calculate Band Power across all time steps and plot
+
+# Since the data has already been FFT'd, need to format into tuple for get_band_power()
+# get_psd() returns a tuple with amplitude and frequency arrays
+# The data is FFT 0-60Hz
+bpData = np.zeros([NUM_TIMESTEPS,NUM_CHANNELS])
+for i in range(NUM_CHANNELS):
+    for t in range(NUM_TIMESTEPS):
+        inTuple = (denoisedData[t,i,:],np.array(range(NUM_FREQS),np.float64))
+        bp = DataFilter.get_band_power(inTuple,0,NUM_FREQS)
+        bpData[t,i] = bp
+
+plt.figure()
+endpoint = NUM_TIMESTEPS/25
+timeVec = np.linspace(0,endpoint,num=NUM_TIMESTEPS)
+for i in range(NUM_CHANNELS):
+    plt.plot(timeVec, bpData[:,i])
+plt.title("Band Power")
+plt.xlabel("Time")
+plt.ylabel("Power")
 
 plt.show()
